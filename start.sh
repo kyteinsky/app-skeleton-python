b6e6a0cfa8f9d0d0984f486250a724f6fc16f66a
Anupam Kumar <kyteinsky@gmail.com>
2025-02-21 14:57:57 +0530

    fix: rewrite frpc.toml on each launch
    Signed-off-by: Anupam Kumar <kyteinsky@gmail.com>

--- start.sh
+++ start.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 set -e
 
-# Check if the configuration file already exists
-if [ -f /frpc.toml ]; then
-    echo "/frpc.toml already exists, skipping creation."
-else
-    # Only create a config file if HP_SHARED_KEY is set.
-    if [ -n "$HP_SHARED_KEY" ]; then
-        echo "HP_SHARED_KEY is set, creating /frpc.toml configuration file..."
-        if [ -d "/certs/frp" ]; then
-            echo "Found /certs/frp directory. Creating configuration with TLS certificates."
-            cat <<EOF > /frpc.toml
+# Only create a config file if HP_SHARED_KEY is set.
+if [ -n "$HP_SHARED_KEY" ]; then
+    echo "HP_SHARED_KEY is set, creating /frpc.toml configuration file..."
+    if [ -d "/certs/frp" ]; then
+        echo "Found /certs/frp directory. Creating configuration with TLS certificates."
+        cat <<EOF > /frpc.toml
 serverAddr = "$HP_FRP_ADDRESS"
 serverPort = $HP_FRP_PORT
 metadatas.token = "$HP_SHARED_KEY"
@@ -25,9 +21,9 @@ localIP = "127.0.0.1"
 localPort = $APP_PORT
 remotePort = $APP_PORT
 EOF
-        else
-            echo "Directory /certs/frp not found. Creating configuration without TLS certificates."
-            cat <<EOF > /frpc.toml
+    else
+        echo "Directory /certs/frp not found. Creating configuration without TLS certificates."
+        cat <<EOF > /frpc.toml
 serverAddr = "$HP_FRP_ADDRESS"
 serverPort = $HP_FRP_PORT
 metadatas.token = "$HP_SHARED_KEY"
@@ -39,10 +35,9 @@ localIP = "127.0.0.1"
 localPort = $APP_PORT
 remotePort = $APP_PORT
 EOF
-        fi
-    else
-        echo "HP_SHARED_KEY is not set. Skipping FRP configuration."
     fi
+else
+    echo "HP_SHARED_KEY is not set. Skipping FRP configuration."
 fi
 
 # If we have a configuration file and the shared key is present, start the FRP client
